3403c7a5191bce4d3a05120837b6ce53aa0d443f
Sulaiman <sulaimain@kobo360.com>
2020-11-12 20:49:37 +0100

    Set up env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  Logger.log(`Server running on http://localhost:${PORT}`)
 }
 bootstrap();
