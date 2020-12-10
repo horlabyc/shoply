ebd65fa835755d836dcb950ba607dcc537fa0329
Sulaiman <sulaimain@kobo360.com>
2020-12-10 14:31:38 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,8 @@ import { AppModule } from './app.module';
 
 const PORT = process.env.PORT || 3002;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true});
   await app.listen(PORT);
-  app.enableCors({
-    origin: [
-      '*'
-    ]
-  })
   Logger.log(`Server running on http://localhost:${PORT}`);
 }
 bootstrap();
